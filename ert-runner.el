7e81208e10577d2c40ef15ee6fcc02dc4f8e6389
Johan Andersson <johan.rejeep@gmail.com>
2014-05-20 08:27:30 +0200

    Release v0.6.3.

--- ert-runner.el
+++ ert-runner.el
@@ -6,7 +6,7 @@
 
 ;; Author: Johan Andersson <johan.rejeep@gmail.com>
 ;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
-;; Version: 0.6.2
+;; Version: 0.6.3
 ;; Keywords: test
 ;; URL: http://github.com/rejeep/ert-runner.el
 ;; Package-Requires: ((s "1.6.1") (dash "1.8.0") (f "0.10.0") (commander "0.2.0") (ansi "0.1.0") (shut-up "0.1.0"))
